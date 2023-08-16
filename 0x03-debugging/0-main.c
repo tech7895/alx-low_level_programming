dffebb6f7f6e6e28e720e5cc4d44e2b4df5e0c24
tech7895 <techhunter990@gmail.com>
2023-08-16 07:25:13 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,12 +5,12 @@
 * Return: 0
 */
 
-init main(void)
+int main(void)
 {
-	int i;
+				int i;
 
-	i = 98;
-	positive_or_negative(i);
+				i = 0;
+				positive_or_negative(i);
 
-	return (0);
+		return (0);
 }
