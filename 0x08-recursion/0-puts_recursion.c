a50f82744a3adf30b70d434a500fa11d1e49bcec
tech7895 <techhunter990@gmail.com>
2023-08-30 02:16:39 -0700

    She locked away a secret, deep inside herself

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - This function prints a strig
+ *
+ * @s: the string
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s  != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+Project
+0x08-recursion
