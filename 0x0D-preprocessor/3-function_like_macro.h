618c90d47c4d2969d7be95170c6a9e5545af51fd
tech7895 <techhunter990@gmail.com>
2023-09-11 03:47:42 -0700

    Function-like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef _MACROLIKE_H_
+#define _MACROLIKE_H_
+
+/**
+ * File: 3-function_like_macro.h
+ *
+ * @x: The input variable
+ *
+ * Desc: header file that computes the absolute value of a number x
+ *
+ */
+#define ABS(x) (x < 0 ? -1 * (x) : (x))
+
+#endif
