7b6ab1e1c6852dd78c3698124c32b850599ab842
tech7895 <techhunter990@gmail.com>
2023-09-11 03:55:56 -0700

    Function-like macro

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,16 @@
+#ifndef _MACRO_H_
+#define _MACRO_H_
+
+/**
+ * File: 4-sum.h
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Desc: This computes the sum of the numbers x and y
+ *
+ */
+#define SUM(a, b) (a + b)
+
+#endif
+
