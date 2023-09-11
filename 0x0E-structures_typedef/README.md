dab26b6a7c87cb1b3157cd92e5529d62adcce875
tech7895 <techhunter990@gmail.com>
2023-09-11 05:08:42 -0700

    Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+This Directory contains the project
+0x0E-structures_typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Define a new type struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Desc - Structure
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
