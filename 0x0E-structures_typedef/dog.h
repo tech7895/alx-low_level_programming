dd8336959765af2e1770d860dca09fc07acb0522
tech7895 <techhunter990@gmail.com>
2023-09-11 06:08:14 -0700

    Outside of a dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,14 +15,15 @@
  *
  */
 
-typedef struct dog dog_t;
+typedef struct dog dog_t
 
-struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
+
 #endif
