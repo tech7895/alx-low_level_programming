f5dc72e8d4a78303fbcb5e5f0f26ea69b966f99f
tech7895 <techhunter990@gmail.com>
2023-09-14 02:07:52 -0700

    Beauty is variable

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,10 @@
+#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stddef.h>
 #include <stdarg.h>
-
-#ifndef VARIADIC_FUNCTIONS
-#define VARIADIC_FUNCTIONS
 
 int sum_them_all(const unsigned int n, ...);
 
