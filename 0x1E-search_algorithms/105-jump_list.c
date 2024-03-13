3972945dd36567133e02f37ac623dbbf56c5a1c2
tech7895 <coper5540@gmail.com>
2024-03-13 06:09:42 -0700

    1

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -8,7 +8,8 @@
  * @value: The value to search for.
  *
  * Return: If the value is not present or the head of the list is NULL, NULL.
- *         else, a the the pointer to the first node where the value is located.
+ *         else, a the the pointer to the first node where
+ *         the value is located.
  *
  * Description: Prints a value every time it is compared in the list.
  *              Uses the square root of the list size as the jump step.
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -48,6 +48,7 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
-
+void print_array(int *array, size_t l, size_t r);
+int binary_search_index(int *array, size_t l, size_t r, int value);
 
 #endif
